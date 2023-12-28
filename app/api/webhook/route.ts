2212112a23299c931b5c7774a786aebf0a335212
DracoR22 <rdraco039@gmail.com>
2023-12-28 16:30:17 -0500

    cors fix in stripe webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -4,7 +4,7 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-export const corsHeaders = {
+const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
